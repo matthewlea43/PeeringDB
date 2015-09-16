891f1aa68e8c020aa28bc139791fc6cba79f9061
Matthew Lea <matthew_lea@hotmail.com>
2015-09-16 21:42:24 +0100

    Update peeringDB.py

--- peeringDB.py
+++ peeringDB.py
@@ -1,52 +1,41 @@
 #!/usr/bin/env python
 
-
-# This script will build peering config based on the peering AS for Linx
-
-
 from sys import argv
 import urllib2
 import json
+import fileinput
+import re
 
 
 
-script, asnum = argv
+asnum = argv[1]
+
+self = 16
+
 url = 'https://beta.peeringdb.com/api/asn/%s' % asnum
 obj = urllib2.urlopen(url)
 raw = json.load(obj)
-self = 16
 
-for x in raw["data"]:
-  maxprefixv4 = x[u'info_prefixes4']
-  maxprefixv6 = x[u'info_prefixes6']
-  name = x[u'name']
-  asn = x[u'asn']
-  org_id = x [u"org_id"]
+
+    
+for item in raw["data"]:
+    maxprefixv4 = item[u'info_prefixes4']
+    maxprefixv6 = item[u'info_prefixes6']
+    name = item[u'name']
+    asn = item[u'asn']
+    org_id = item[u"org_id"]
 
 for item in raw["data"] [0] ["ixlink_set"]:
         if item["ixlan_id"] == self:
                 ipaddr4 = item[u"ipaddr4"]
                 ipaddr6 = item[u"ipaddr6"]
 
-print "!"
-print "router bgp 51551"
-print "!"
-print "neighbor", ipaddr4, "remote-as",asn
-print "neighbor", ipaddr4, "peer-group LINX-IXP-IPV4"
-print "neighbor", ipaddr4, "description",name
-print "neighbor", ipaddr6, "remote-as",asn
-print "neighbor", ipaddr6, "peer-group LINX-IXP-IPV4"
-print "neighbor", ipaddr6, "description",name
-print "!"
-print "address-family ipv4"
-print "neighbor", ipaddr4, "activate"
-print "neighbor", ipaddr4, "inherit peer-policy LINX-IXP-IPV4"
-print "neighbor", ipaddr4, "maximum-prefix",maxprefixv4
-print "neighbor", ipaddr4, "description",name
-print "!"
-print "address-family ipv6"
-print "neighbor", ipaddr6, "activate"
-print "neighbor", ipaddr6, "inherit peer-policy LINX-IXP-IPV6"
-print "neighbor", ipaddr6, "maximum-prefix",maxprefixv6
-print "neighbor", ipaddr6, "description",name
-print "!"
+
+with open('cisco-ios.txt', 'r') as file:
+    file_str = file.read()
+    new_file = file_str.replace('$ipaddr4',ipaddr4).replace('$ipaddr6',ipaddr6).replace('$name',name).replace('$maxprefixv4',str(maxprefixv4)).replace('$maxprefixv6',str(maxprefixv6)).replace('$asn',str(asnum))
+    file.close()
+    
+fo = open(asnum + "-linx.cfg", "wb")
+fo.write(new_file)
+fo.close()
